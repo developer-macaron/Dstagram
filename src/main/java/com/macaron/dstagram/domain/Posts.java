33c1864aa03dd0e740dd02e97eea32b3cf6889a3
jeon_yejin <yemong1226@gmaill.com>
2022-03-24 14:46:35 +0900

    mysql 설정 추가

---
+++ src/main/java/com/macaron/dstagram/domain/Posts.java
@@ -0,0 +1,16 @@
+package com.macaron.dstagram.domain;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+}
