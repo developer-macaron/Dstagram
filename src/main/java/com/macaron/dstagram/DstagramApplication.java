9703d5bddb0dc0e212f9255014d53e23ff2701fe
jeon_yejin <yemong1226@gmaill.com>
2022-03-24 09:48:28 +0900

    Initial commit

---
+++ src/main/java/com/macaron/dstagram/DstagramApplication.java
@@ -0,0 +1,13 @@
+package com.macaron.dstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/macaron/dstagram/DstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macaron.dstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
